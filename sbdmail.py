64254704079fe4e50b4d2fbe2bed6562435bf9f1
MisterMartin <sugartechllc@gmail.com>
2023-04-11 08:18:48 +1200

    Fix up the help message

--- sbdmail.py
+++ sbdmail.py
@@ -2,6 +2,7 @@ import os
 import signal
 import sys
 from argparse import ArgumentParser
+from argparse import RawTextHelpFormatter
 from ConfigIni import ConfigIni
 from SbdProcessor import SbdProcessor
 
@@ -30,27 +31,37 @@ if __name__ == "__main__":
         config.save()
 
         description = f'''
+Fetch recent Iridium email messages and decode them as text or json.
+If --keep is specified, the message attachments will be saved:
+python3 sbdmail.py -b 9-apr-2023 11-apr-2023 -n 1
+
 Arguments not specified on the command line are taken from
 {config.configPath()}.
-Edit that file to set default values. Be careful of sharing it,
-if account/password details are specified there.
 
-Run the program once to create the initial .ini file,
-Then edit that file to set your chosen defaults.
+Run the program once to create the initial .ini file, (it will give errors),
+and then edit {config.configPath()}
+**Be careful of sharing this file, if account/password details
+are specified there.**
 '''
 
-        parser = ArgumentParser(description=description)
-        parser.add_argument("-a", "--account", help="email account",                 action="store", default=config.values()['Main']['EmailAccount'])
-        parser.add_argument("-p", "--password",help="mailbox password",              action="store", default=config.values()['Main']['EmailPassword'])
-        parser.add_argument("-t", "--subject", help="subject contains [SBD Msg]",    action="store", default=config.values()['Main']['SearchSubject'])
-        parser.add_argument("-b", "--begin",   help="since date (d-MMM-yyyy) UTC (e.g. 1-jan-2023)",   action="store")
-        parser.add_argument("-e", "--end",     help="end date (d-MMM-yyyy) UTC (e.g. 1-jan-2023, does not include this day)", action="store")
-        parser.add_argument("-i", "--imap",    help="imap server [imap.gmail.com]",  action="store", default=config.values()['Main']['ImapServer'])
-        parser.add_argument("-n", "--number",  help="report the last NUMBER msgs (0==all)",   action="store", default=0, type=int)
-        parser.add_argument("-k", "--keep",    help="keep message in directory",     action="store", default=os.path.expanduser(config.values()['Main']['KeepFilesDirectory']))
-        parser.add_argument('-j', '--json',    help='output json instead of text',   action='store_true')
-        parser.add_argument('-r', '--repeat',  help='repeat the down load after REPEAT seconds (0 == no repeat)', action='store', default=0, type=int)
-        parser.add_argument("-v", "--verbose", help="verbose",                       action="store_true", default = False)
+        epilog = f'''
+Note: some arguments may already be specified in {config.configPath()}.
+'''
+
+        parser = ArgumentParser(description=description, epilog=epilog, formatter_class=RawTextHelpFormatter)
+        required = parser.add_argument_group('required arguments')
+        required.add_argument("-a", "--account", help="email account",                 action="store", default=config.values()['Main']['EmailAccount'])
+        required.add_argument("-p", "--password",help="mailbox password",              action="store", default=config.values()['Main']['EmailPassword'])
+        required.add_argument("-t", "--subject", help="subject contains [SBD Msg]",    action="store", default=config.values()['Main']['SearchSubject'])
+        required.add_argument("-b", "--begin",   help="since date (d-MMM-yyyy) UTC (e.g. 1-jan-2023)",   action="store")
+        required.add_argument("-e", "--end",     help="end date (d-MMM-yyyy) UTC (e.g. 1-jan-2023, does not include this day)", action="store")
+        required.add_argument("-i", "--imap",    help="imap server [imap.gmail.com]",  action="store", default=config.values()['Main']['ImapServer'])
+        optional = parser.add_argument_group('optional arguments')
+        optional.add_argument("-n", "--number",  help="(optional) report the last NUMBER msgs (0==all)",   action="store", default=0, type=int)
+        optional.add_argument("-k", "--keep",    help="(optional) keep message in directory",     action="store", default=os.path.expanduser(config.values()['Main']['KeepFilesDirectory']))
+        optional.add_argument('-j', '--json',    help='(optional) output json instead of text',   action='store_true')
+        optional.add_argument('-r', '--repeat',  help='(optional) repeat the download after REPEAT seconds (0 == no repeat)', action='store', default=0, type=int)
+        optional.add_argument("-v", "--verbose", help="(optional) verbose",                       action="store_true", default = False)
         args = parser.parse_args()
 
         if not (args.account and args.password and args.begin and args.end):
